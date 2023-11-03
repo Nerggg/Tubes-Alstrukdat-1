5be01f3bf66a926876aa02865d5e2abbc051fff7
Maharanish <shabrina.maharani22@gmail.com>
2023-11-03 19:14:45 +0700

    kicau

--- src/adt/kicauan.h
+++ src/adt/kicauan.h
@@ -4,11 +4,7 @@
 #include <stdio.h>
 #include "pengguna.h"
 #include "datetime.h"
-#include "listlinier.h"
-
-
-// Konstanta
-#define MAX_Length_text 280 //termasuk tanda baca dan spasi
+#include "listdin.h"
 
 // Definisi tipe data kicauan
 typedef struct {
@@ -19,21 +15,10 @@ typedef struct {
 	DATETIME datetime;
 } Kicau;
 
-typedef struct node* Address;
-typedef struct listkicau {
-    Kicau info;
-    struct listkicau* next; 
-} listkicau;
-
 #define IDX_UNDEF (-1)
 #define FIRST(l) (l)
 
-Address newNodekicau(Kicau kicauanlokasibaru);
-void insertKicauanFirst(listkicau *kicauan, Kicau kicauanbaru);
-void kicau(Word *currentUser);
-List kicauan(List kicauan);
-Kicau displaykicauan(Kicau *kicauan);
-Kicau suka_kicauan(int id);
-void ubah_kicauan(int id);
+
+
 
 #endif
