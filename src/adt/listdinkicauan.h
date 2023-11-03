bd9279f0e3a61d4104315f6fde9b865440b08f18
Maharanish <shabrina.maharani22@gmail.com>
2023-11-03 22:34:13 +0700

    listdinkicauan.h

---
+++ src/adt/listdinkicauan.h
@@ -0,0 +1,94 @@
+/* MODUL INTEGER DYNAMIC LIST */
+/* Berisi definisi dan semua primitif pemrosesan list integer */
+/* Penempatan elemen selalu rapat kiri */
+/* Versi II : dengan banyaknya elemen didefinisikan secara eksplisit,
+   memori list dinamik */
+
+#ifndef LISTDIN_H
+#define LISTDIN_H
+
+#include "boolean.h"
+#include "kicauan.h"
+#include "listdin.h"
+
+/*  Kamus Umum */
+#define IDX_MIN 0
+/* Indeks minimum list */
+#define IDX_UNDEF -1
+/* Indeks tak terdefinisi*/
+
+/* Definisi elemen dan koleksi objek */
+
+typedef int IdxType;
+typedef struct
+{
+    Kicau *buffer; /* memori tempat penyimpan elemen (container) */
+    int nEff;       /* >=0, banyaknya elemen efektif */
+    int capacity;   /* ukuran elemen */
+} ListDinkicau;
+/* Indeks yang digunakan [0..capacity-1] */
+/* Jika l adalah : ListDin, cara deklarasi dan akses: */
+/* Deklarasi : l : ListDin */
+/* Maka cara akses:
+   l.nEff      untuk mengetahui banyaknya elemen
+   l.buffer    untuk mengakses seluruh nilai elemen list
+   l.buffer[i] untuk mengakses elemen ke-i */
+/* Definisi :
+  list kosong: l.nEff = 0
+  Definisi elemen pertama : l.buffer[i] dengan i=0
+  Definisi elemen terakhir yang terdefinisi: l.buffer[i] dengan i=l.capacity */
+
+/* ********** SELEKTOR ********** */
+#define NEFF(l) (l).nEff
+#define BUFFER(l) (l).buffer
+#define ELMT(l, i) (l).buffer[i]
+#define CAPACITY(l) (l).capacity
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create list kosong  */
+void CreateListDinkicau(ListDinkicau *l, int capacity);
+/* I.S. l sembarang, capacity > 0 */
+/* F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity */
+
+void dealocateListkicau(ListDinkicau *l);
+/* I.S. l terdefinisi; */
+/* F.S. (l) dikembalikan ke system, CAPACITY(l)=0; NEFF(l)=0 */
+
+void printListkicau(ListDinkicau l);
+/* Proses : Menuliskan isi list dengan traversal, list ditulis di antara kurung siku;
+   antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan karakter di depan,
+   di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. l boleh kosong */
+/* F.S. Jika l tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika list kosong : menulis [] */
+
+
+
+
+/* ********** OPERASI LAIN ********** */
+void copyListkicau(ListDinkicau lIn, ListDinkicau *lOut);
+/* I.S. lIn terdefinisi tidak kosong, lOut sembarang */
+/* F.S. lOut berisi salinan dari lIn (identik, nEff dan capacity sama) */
+/* Proses : Menyalin isi lIn ke lOut */ 
+
+
+
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertLastkicau(ListDinkicau *l, Kicau newkicauan);
+/* Proses: Menambahkan val sebagai elemen terakhir list */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen terakhir l yang baru */
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteLastkicau(ListDinkicau *l, Kicau *kicauantemp);
+/* Proses : Menghapus elemen terakhir list */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+/*      Banyaknya elemen list berkurang satu */
+/*      List l mungkin menjadi kosong */
+
+ListDinkicau *cariKicauan(ListDinkicau *l,int id);
+
+#endif
