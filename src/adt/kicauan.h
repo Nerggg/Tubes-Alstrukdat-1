52591a91d76a8abf6e7db36842e767fe17857882
Shabrina Maharani <130067945+Maharanish@users.noreply.github.com>
2023-11-12 21:09:01 +0700

    src : header kicauan.h

--- src/adt/kicauan.h
+++ src/adt/kicauan.h
@@ -1,7 +1,6 @@
 #ifndef KICAU_H
 #define KICAU_H
 
-#include <stdio.h>
 #include "pengguna.h"
 #include "datetime.h"
 
@@ -22,6 +21,71 @@ void kicau(UserDB *user,Word *currentUser);
 Kicau displaykicauan(Kicau *kicauan);
 Kicau suka_kicauan(UserDB *user,int id);
 void ubah_kicauan(UserDB *user,int id);
+
+/*  Kamus Umum */
+#define IDX_MIN 0
+/* Indeks minimum list */
+/* Indeks tak terdefinisi*/
+
+/* Definisi elemen dan koleksi objek */
+typedef struct
+{
+    Kicau *buff; /* memori tempat penyimpan elemen (container) */
+    int neff;       /* >=0, banyaknya elemen efektif */
+    int CAPACITY;   /* ukuran elemen */
+} ListDinkicau;
+/* Indeks yang digunakan [0..capacity-1] */
+/* Jika l adalah : ListDin, cara deklarasi dan akses: */
+/* Deklarasi : l : ListDin */
+/* Maka cara akses:
+   l.nEff      untuk mengetahui banyaknya elemen
+   l.buffer    untuk mengakses seluruh nilai elemen list
+   l.buffer[i] untuk mengakses elemen ke-i */
+/* Definisi :
+  list kosong: l.nEff = 0
+  Definisi elemen pertama : l.buffer[i] dengan i=0
+  Definisi elemen terakhir yang terdefinisi: l.buffer[i] dengan i=l.capacity */
+
+/* ********** SELEKTOR ********** */
+#define NEFF(l) (l).neff
+#define BUFFER(l) (l).buff
+#define ELMT(l, i) (l).buff[i]
+#define CAPACITY(l) (l).CAPACITY
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create list kosong  */
+void CreateListDinkicau(ListDinkicau *l, int cap);
+/* I.S. l sembarang, capacity > 0 */
+/* F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity */
+
+void dealocateListkicau(ListDinkicau *l);
+/* I.S. l terdefinisi; */
+/* F.S. (l) dikembalikan ke system, CAPACITY(l)=0; NEFF(l)=0 */
+
+/* ********** OPERASI LAIN ********** */
+void copyListkicau(ListDinkicau lIn, ListDinkicau *lOut);
+/* I.S. lIn terdefinisi tidak kosong, lOut sembarang */
+/* F.S. lOut berisi salinan dari lIn (identik, nEff dan capacity sama) */
+/* Proses : Menyalin isi lIn ke lOut */ 
+
+
+
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertLastkicau(ListDinkicau *l, Kicau newkicauan);
+/* Proses: Menambahkan val sebagai elemen terakhir list */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen terakhir l yang baru */
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteLastkicau(ListDinkicau *l, Kicau *kicauantemp);
+/* Proses : Menghapus elemen terakhir list */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+/*      Banyaknya elemen list berkurang satu */
+/*      List l mungkin menjadi kosong */
+
+ListDinkicau *cariKicauan(ListDinkicau *l,int id);
 
 
 
